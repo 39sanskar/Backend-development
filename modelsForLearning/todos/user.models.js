26e9921484401d809bf324e14efbb5a053210749
39sanskar <mishrasanskar496@gmail.com>
2025-08-26 12:33:38 +0530

    usermodel

---
+++ modelsForLearning/todos/user.models.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    }, // when you define username in a object then you call super-power of the mongoose
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true,
+    }
+
+  }, { timestamps: true }
+)
+
+export const User = mongoose.model("User", userSchema)
